a3d2766e2a6ecb2310a8a44819232db68b91a840
Gilles Wittenberg <git@gilleswittenberg.com>
2014-04-24 13:27:25 +0200

    Use jasmine-jquery version 1.7 to fix tests

--- karma.conf.js
+++ karma.conf.js
@@ -5,7 +5,7 @@ module.exports = function (config) {
       'http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js',
       'http://cdnjs.cloudflare.com/ajax/libs/underscore.js/1.5.2/underscore.js',
       'http://cdnjs.cloudflare.com/ajax/libs/backbone.js/1.1.0/backbone.js',
-      'https://raw.github.com/velesin/jasmine-jquery/master/lib/jasmine-jquery.js',
+      'bower_components/jasmine-jquery/lib/jasmine-jquery.js',
       'backbone.lightbox.js',
       'test/*.spec.js'
     ],
